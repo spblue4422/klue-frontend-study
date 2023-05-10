2de8ac4615bba92b9cb7827da65f201f49076cf7
unknown <spblue4422@naver.com>
2023-05-10 22:40:27 +0900

    w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,34 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { MouseEventHandler, useState } from "react";
 import Square from "../tictactoe/Square";
 
 export default function Home() {
+	const [squares, setSquares] = useState(Array(9).fill(undefined));
+
+	const buttonClick = (order: number) => {
+		const nextSquares = squares.slice();
+		nextSquares[order] = 'X';
+		setSquares(nextSquares);
+	};
+
 	return (
 		<main>
 			<div>
 				<div className={"board-row"}>
-					<Square value={1}></Square>
-					<Square value={2}></Square>
-					<Square value={3}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[0]} order={1}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[1]} order={2}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[2]} order={3}></Square>
 				</div>
 
 				<div className={"board-row"}>
-					<Square value={4}></Square>
-					<Square value={5}></Square>
-					<Square value={6}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[3]} order={4}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[4]} order={5}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[5]} order={6}></Square>
 				</div>
 				<div className={"board-row"}>
-					<Square value={7}></Square>
-					<Square value={8}></Square>
-					<Square value={9}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[6]} order={7}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[7]} order={8}></Square>
+					<Square onSquareClick={() => buttonClick} value={squares[8]} order={9}></Square>
 				</div>
 			</div>
 		</main>
--- src/tictactoe/Square.tsx
+++ src/tictactoe/Square.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { ButtonHTMLAttributes, EventHandler, MouseEventHandler, useState } from "react";
 import "../app/style.css";
 
 interface SquareProps {
+	onSquareClick : () => void;
 	value: number;
+	order: number;
 }
 
-const Square: React.FC<SquareProps> = ({ value }) => {
-	return <button className={"square"}>{value}</button>;
+const Square: React.FC<SquareProps> = ({ onSquareClick, value, order }) => {
+	const buttonHandle = () => {
+		onSquareClick;
+	}
+
+	return (
+		<button className={"square"} onClick={onSquareClick}>
+			{value}
+		</button>
+	);
 };
 
 export default Square;
